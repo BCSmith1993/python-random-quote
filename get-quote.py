1d660d2d77759683f486be1ac5f3185b28c76dd9
BCSmith1993 <bcsmith1950@gmail.com>
2021-03-02 10:23:42 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def primary():
-   print(This is nifty!)
+   #print(This is nifty!)
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   primary()
